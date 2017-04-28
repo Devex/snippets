7c95523ac15a6f63af98a60e2d29931d0e6bd344
Nicolas Fricke <nicolas.fricke@devex.com>
2017-04-28 18:17:02 +0200

    Add initial scripts
    These are scripts I have been using locally for a while already.
    Adding them to the repo for easier sharing.

---
+++ pull_repos.sh
@@ -0,0 +1,1 @@
+(cd ~/work/grape-api && git pull --rebase devex develop && cd ~/work/front-end && git pull --rebase devex develop)
---
+++ restore_vagrant.sh
@@ -0,0 +1,4 @@
+rm -rf .vagrant/machines/default/virtualbox/id.bkp
+mv .vagrant/machines/default/virtualbox/id .vagrant/machines/default/virtualbox/id.bkp 2>/dev/null || true
+echo "3e1cf2dc-0f80-4af6-843b-451ba04ddd9f" > .vagrant/machines/default/virtualbox/id
+cp ~/.ssh/id_rsa.pub .vagrant/machines/default/virtualbox/private_key
