c018eac9ac082fba84767695d780f9b8bb77991f
Manu Campos <manu@invisibles.cc>
2017-05-02 16:11:33 +0200

    Create manu_aliases.sh

---
+++ manu_aliases.sh
@@ -0,0 +1,10 @@
+alias rspec="bundle exec rspec"
+alias rake="bundle exec rake"
+alias be="bundle exec"
+alias grc="bundle exec racksh" # Grape Console
+alias gr="bundle exec rackup -p 3004" # Grape
+alias fr="bundle exec rails s -p 3002" # Frontend
+alias frc="bundle exec rails c" # Frontend Cosnole
+alias dbm="rake db:migrate"
+alias tdbm="RACK_ENV=test rake db:migrate"
+alias mux="tmuxinator"
